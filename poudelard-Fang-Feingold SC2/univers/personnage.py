ca3f93812522261dfe767692f21e1da0c054840c
Monica <fangmonica2007@gmail.com>
2025-12-03 15:11:21 +0100

    fonction init perso

---
+++ poudelard-Fang-Feingold SC2/univers/personnage.py
@@ -0,0 +1,11 @@
+def initialiser_personnage(nom, prenom, attributs):
+    return {
+        "Nom": nom,
+        "Prenom": prenom,
+        "Argent": 100,
+        "Inventaire": [],
+        "Sortilèges": [],
+        "Attributs": attributs
+    }
+
+
