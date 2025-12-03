28e632948670791ad06990c1488f6e484d313676
Monica <fangmonica2007@gmail.com>
2025-12-03 14:42:23 +0100

    fonction demander_nombre

--- poudelard-Fang-Feingold SC2/utils/input_utils.py
+++ poudelard-Fang-Feingold SC2/utils/input_utils.py
@@ -4,3 +4,10 @@ def demander_texte(message):
         txt = input("veuillez écrire quelque chose")
     else:
         return txt
+
+def demander_nombre(message, min_Val = None, max_Val = None):
+    nombre = input(message)
+    while nombre.strip() == "":
+        nombre = input("Veuillez entrez un nombre valide")
+    else:
+        return nombre
