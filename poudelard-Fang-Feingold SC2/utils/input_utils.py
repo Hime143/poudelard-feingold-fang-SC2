65ce9799ff3385b108339d30424af3f91ff99314
Hime143 <cleo.feingold@gmail.com>
2025-12-03 14:41:54 +0100

    fonction demander texte

--- poudelard-Fang-Feingold SC2/utils/input_utils.py
+++ poudelard-Fang-Feingold SC2/utils/input_utils.py
@@ -1,6 +1,6 @@
 def demander_texte(message):
     txt = input(message)
     while txt.strip() == "":
-        txt = input("veuillez écrire quelque chose")
+        txt = input(message)
     else:
         return txt
