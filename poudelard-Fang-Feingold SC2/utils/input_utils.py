c8d2697bd919836d35423a15358ad43dfd202b2c
Hime143 <cleo.feingold@gmail.com>
2025-12-03 14:50:45 +0100

    fonction demander choix

--- poudelard-Fang-Feingold SC2/utils/input_utils.py
+++ poudelard-Fang-Feingold SC2/utils/input_utils.py
@@ -11,3 +11,10 @@ def demander_nombre(message, min_Val = None, max_Val = None):
         nombre = input("Veuillez entrez un nombre valide")
     else:
         return nombre
+
+def demander_choix(message, options):
+    print(message)
+    for i in range(len(options)):
+        print("{}. {}".format(i+1, options[i]))
+    choix = demander_nombre("votre choix : ", 1, len(options))
+    return choix
